7429bb655089c69019e3951540491b8795eb1e2f
AntekSzczukiewicz <91031184+AntekSzczukiewicz@users.noreply.github.com>
2023-08-03 13:32:25 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ import pandas as pd
 import os, re
 import time
 
-import time
-
-name_list = ["temperature", "humidity", "particle_5", "particle_0x5"]
-
+#ustawienie sciezki
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+
+name_list = ["temperature", "humidity", "particle_5", "particle_0x5"]
+#wczytanie nazw plikow z folderu pliki
+filenames = os.listdir('pliki/')
 
 def data_import(path):
     df = pd.read_csv(path, sep=";", nrows=50000) ### USTAWIC NA 50000 ###
@@ -18,17 +19,15 @@ def data_import(path):
     df = df[["TimeString", "Time_ms", "VarValue"]]
     return df
 
-#wczytanie nazw plikow z folderu pliki
-filenames = os.listdir('pliki/')
+
+
 
 def get_dataframe(komora, data_type):
-    pattern = re.compile(f"k{komora}_{data_type}*")
+    pattern = re.compile(f"^k{komora}_{data_type}")
     names = [name for name in filenames if pattern.match(name)]
     dfs = [data_import(path = f"pliki/{name}") for name in names]
     if dfs:
         df = pd.concat(dfs, axis=0)
-        #konwertowanie TYLKO DATA
-        #df["TimeString"] = pd.to_datetime(df["TimeString"], format=r"%d.%m.%Y %H:%M:%S")
         return df
     return pd.DataFrame()
 
@@ -36,7 +35,7 @@ def import_chamber(number):
     D = {}
     for n in name_list:
         D[n] = get_dataframe(number, n)
-    
+
     th = pd.merge(D["temperature"], D["humidity"], on=["TimeString"], how="inner")
     th = th.rename(columns={"VarValue_x" : "Temperature", "VarValue_y" : "Humidity", "Time_ms_x" : "Time_ms"})
     th = th.drop("Time_ms_y", axis=1)
@@ -51,19 +50,56 @@ def import_chamber(number):
     RD = {"th" : th, "p" : p}
     return RD
 
-#Filtrowanie warunkiem
-K1 = import_chamber(1)
-th = K1["th"]
-
-print(th[(th["TimeString"] >= pd.Timestamp(2023, 7, 28, 10, 40))])
+CHAMBERS = {1 : import_chamber(1), 2 : import_chamber(2), 3 : import_chamber(3), 4 : import_chamber(4)}
 
 class Day:
-    def __init__(self, start, stop):
+    #TH 
+    def __init__(self, start, stop, active_chambers):
         self.start = start
         self.stop = stop
+        self.active_chambers = active_chambers
+        self.D = {}
+        self.get_all_data()
+        self.analyze()
 
-    def show(self, df):
-        return df[(df["TimeString"] >= self.start) & (df["TimeString"] <= self.stop)]
-    
-d1 = Day(pd.Timestamp(2023, 7, 28, 10, 40), pd.Timestamp(2023, 7, 28, 10, 43))
-d1.show(th)
+    def get_th(self, df):
+        self.th = df[(df["TimeString"] >= self.start) & (df["TimeString"] <= self.stop)]
+        self.th.loc[:, "Temperature"] = self.th["Temperature"].apply(lambda x : float(x.replace(",", ".")))
+        self.th.loc[:, "Humidity"] = self.th["Humidity"].apply(lambda x : float(x.replace(",", ".")))
+        return self.th
+
+    def get_p(self, df):
+        self.p = df[(df["TimeString"] >= self.start) & (df["TimeString"] <= self.stop)]
+        return self.p
+
+    def get_chamber(self, K):
+        return {"th" : self.get_th(K["th"]), "p" : self.get_p(K["p"])}
+
+    def get_all_data(self):
+        for k in self.active_chambers:
+            self.D[k] = self.get_chamber(CHAMBERS[1])
+        return self.D
+
+    def chamber_analyze(self, n):
+        chamber = self.D[n]
+        S = {}
+        S["temp_max"] = max(chamber["th"]["Temperature"])
+        S["temp_min"] = min(chamber["th"]["Temperature"])
+        S["temp_mean"] = chamber["th"]["Temperature"].mean()
+
+        S["hum_max"] = max(chamber["th"]["Humidity"])
+        S["hum_min"] = min(chamber["th"]["Humidity"])
+        S["hum_mean"] = chamber["th"]["Humidity"].mean()
+
+        S["max_5"] = max(chamber["p"]["particle_5"])
+        S["max_0x5"] = max(chamber["p"]["particle_0x5"])
+        self.D[n]["analysis"] = S
+        return S
+
+    def analyze(self):
+        for k in self.D.keys():
+            self.chamber_analyze(k)
+
+d1 = Day(pd.Timestamp(2023, 7, 14, 12, 25), pd.Timestamp(2023, 7, 14, 12, 55), [1, 2, 3, 4])
+
+print(d1.D)
